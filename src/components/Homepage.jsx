19aaec55e06e538bd6cc1227f9b39bd1788fb858
Thinh-Phung <phunghuuthinh01@gmail.com>
2022-12-31 13:36:22 +0700

    add Cryptocurrencies

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,9 +4,11 @@ import { Typography, Row, Col, Statistic } from "antd";
 
 import { useGetCryptosQuery } from "../services/cryptoApi";
 import { Link } from "react-router-dom";
+import Cryptocurrencies from "./Cryptocurrencies";
+import News from "./News";
 const { Title } = Typography;
 export default function Homepage() {
-  const { data, isFetching } = useGetCryptosQuery();
+  const { data, isFetching } = useGetCryptosQuery(10);
   const globalStats = data?.data?.stats;
   if (isFetching) return "Loading ...";
   return (
@@ -43,6 +45,24 @@ export default function Homepage() {
           />
         </Col>
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Top 10 Cryptocurrencies in the world
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/cryptocurrencies">Show More</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Latest Crypto News
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/news">Show More</Link>
+        </Title>
+      </div>
+      <News simplified />
     </>
   );
 }
